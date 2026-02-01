059bf1a73af5485cf9433a4944aee93bea3eee80
Shamsundar-M-S <shamsundar.sureshmani@gmail.com>
2026-02-01 16:34:10 +0530

    summa da ebba

--- Doublylinked.cpp
+++ Doublylinked.cpp
@@ -49,11 +49,70 @@ class Doubly{
         }
         //insert at given pos
         void insertAtPos(int val, int pos){
+            Node * newnode = new Node(val);
+            Node * curr= head;
+            if (!head ||pos <1){
+                cout<<"invalid"<<endl;
+            }
+            int count=1;
+            while(count<pos-1 && curr){
+                curr=curr->next;
+            }
+            newnode->next=curr->next;
+            newnode->prev=curr;
+            if(curr->next){
+                curr->next->prev=newnode;
+
+            }
+            curr->next=newnode;
+            cout<<"inserted at given position"<<endl;
+        }       
+        //insert after given element
+        void insertafternode(int val,int daata){
+            Node * newnode= new Node(val);
+            Node * curr= head;
+            if(!head){
+                cout<<"invalid"<<endl;
+            }
+            while(curr && curr->data!=daata){
+                curr=curr->next;
+            }
+            newnode->prev=curr;
+            newnode->next=curr->next;
+            if(curr->next){
+                curr->next->prev=newnode;
+            }
+            curr->next=newnode;
+        }
+        //insert befoe given element
+        void insertbeforeelement(int val,int DATA){
             Node * newnode= new Node(val);
             Node * curr=head;
-            if (!head || pos<1){
-                cout<<"INVALID"<<endl;
+            if(!head){
+                cout<<"invalid"<<endl;
             }
-        }       
+            while(curr && curr->data==DATA){
+                curr=curr->next;
+            }
+            newnode->next=curr;
+            newnode->prev=curr->prev;
+            curr->prev->next=newnode;
+            curr->prev=newnode;
+
+        }
+        //delete from begining
+        void deletefromfront(){
+            if (!head){
+                cout<<"invalid"<<endl;
+            }
+            Node * temp=head;
+            head=head->next;
+            head->prev=NULL;
+            delete temp;
+        }
+        //delete from the end 
+        void deletefromend(){
+            
+        }
 
 };
